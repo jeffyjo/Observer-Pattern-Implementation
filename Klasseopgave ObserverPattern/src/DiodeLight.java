7484ee1c8857b09a09a5cc7c08992b3e4988709d
Jeff Jørgensen <jeffjorgensen@Jeffs-MacBook-Air.local>
2016-02-15 12:45:39 +0100

    DiodeLight class

---
+++ Klasseopgave ObserverPattern/src/DiodeLight.java
@@ -0,0 +1,23 @@
+/**
+ * Created by jeffjorgensen on 15/02/2016.
+ */
+public class DiodeLight implements SimpleObserver{
+
+    public DiodeLight(SimpleObserver sob) {
+        ((SimpleObservable) sob).registerObserver(this);
+
+    }
+
+    @Override
+    public void update(SimpleObservable obj) {
+        if (((ElectronicDevice) obj).getState().equals("on")){
+            System.out.println("The diode is now green");
+        } else if (((ElectronicDevice) obj).getState().equals("hibernate")){
+            System.out.println("The diode is now red");
+        } else if (((ElectronicDevice) obj).getState().equals("off")){
+            System.out.println("The diode is turned off");
+        } else {
+            System.out.println("What the F...");
+        }
+    }
+}
