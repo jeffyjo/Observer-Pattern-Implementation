984d6746c130d73927420d633cf673eef8ebeeb3
Denn1593 <oowej@hotmail.com>
2016-02-15 12:35:38 +0100

    update electronic

--- Klasseopgave ObserverPattern/src/ElectronicDevice.java
+++ Klasseopgave ObserverPattern/src/ElectronicDevice.java
@@ -1,12 +1,36 @@
+import java.util.ArrayList;
+
 /**
  * Created by dennis on 2/15/16.
  */
 public class ElectronicDevice implements SimpleObservable
 {
+    ArrayList<SimpleObserver> observers = new ArrayList<SimpleObserver>();
     private String state = "";
 
     public String getState()
     {
         return state;
+    }
+
+    @Override
+    public void registerObserver(SimpleObserver obj)
+    {
+        observers.add(obj);
+    }
+
+    @Override
+    public void removeObserver(SimpleObserver obj)
+    {
+        observers.remove(obj);
+    }
+
+    @Override
+    public void notifyObservers()
+    {
+        for(int i = 0; i < observers.size(); i++)
+        {
+            observers.get(i).update(this);
+        }
     }
 }
