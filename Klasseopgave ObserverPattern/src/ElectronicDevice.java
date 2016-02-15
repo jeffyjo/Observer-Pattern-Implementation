254d2bf893ef96d24411df00eb99edc9c9c30f43
Denn1593 <oowej@hotmail.com>
2016-02-15 12:23:10 +0100

    electronice device

---
+++ Klasseopgave ObserverPattern/src/ElectronicDevice.java
@@ -0,0 +1,12 @@
+/**
+ * Created by dennis on 2/15/16.
+ */
+public class ElectronicDevice implements SimpleObservable
+{
+    private String state = "";
+
+    public String getState()
+    {
+        return state;
+    }
+}
